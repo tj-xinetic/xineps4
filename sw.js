bca0b2d40053a98bbcb71695f7d8af9df4ecf9ba
tj-xinetic <tjdatacore@gmail.com>
2026-04-02 21:02:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xineps4-v1.0.1';
+const CACHE_NAME = 'xineps4-v1.0.2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (e) => {
